543662a71fc8d134ac9bcd62138931cc0d88b773
MESRAR Mahmoud <mahmoud.mesrar@cegedim.com>
2023-01-19 01:01:48 +0100

    add client

--- Partie_B_C/projet_relever/src/main/java/ServerJWS.java
+++
@@ -1,8 +0,0 @@
-import jakarta.xml.ws.Endpoint;
-import org.example.ReleverService;
-
-public class ServerJWS {
-    public static void main(String[] args) {
-        Endpoint.publish("http://localhost:9191/", new ReleverService());
-    }
-}
